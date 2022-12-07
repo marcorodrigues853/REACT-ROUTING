291d93e3c1f59f37feb19331e27ddf3886b4306c
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-12-07 23:09:37 +0000

    teste

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,12 @@
+import { useParams } from 'react-router-dom';
 const Blog = () => {
+  const params = useParams();
+  const { id } = useParams();
+  console.log(params);
   return (
     <>
       <h1>Blog 1</h1>
+      <h1>{id}</h1>
     </>
   );
 };
